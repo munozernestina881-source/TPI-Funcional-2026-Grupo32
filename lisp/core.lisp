90be4ce6129793b39ef316eab19b77e241fccac5
Ernestina del Carmen Muñoz <munozernestina881@gmail.com>
2026-06-08 23:17:08 -0300

    Requerimiento 1: Estados de Transición

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,23 @@
+;; ========================================================
+;; FUNCIÓN: transicion
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Condicional (case)
+;; IMPACTO: No destructiva
+;; ========================================================
 
+(defun transicion (color-actual cambiar-a)
+  (cond
+    ((and (eq color-actual 'en-rojo)
+          (eq cambiar-a 'verde))
+     (list 'en-rojo "cambiar-a-verde"))
+
+    ((and (eq color-actual 'en-verde)
+          (eq cambiar-a 'amarillo))
+     (list 'en-verde "cambiar-a-amarillo"))
+
+    ((and (eq color-actual 'en-amarillo)
+          (eq cambiar-a 'rojo))
+     (list 'en-amarillo "cambiar-a-rojo"))
+
+    (t
+     (list color-actual 'accion-por-defecto))))
