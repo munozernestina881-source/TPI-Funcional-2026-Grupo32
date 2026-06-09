5676041ba4aba7a4bed7dfc3a09cbe309e5fbdd6
LuisBedoya-svg <juanluisbedoya@gmail.com>
2026-06-09 14:14:19 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -37,3 +37,37 @@
       ((< posicion 90) 'rojo)
       ((< posicion 96) 'amarillo)
       (t 'verde))))
+
+;; ========================================================
+;; FUNCIÓN: calcularTiempo
+;; NATURALEZA: Impura
+;; ESTRATEGIA: Codicional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun calcularTiempo (tiempo color-actual)
+(let ((listaColor (transicion color-actual (timer tiempo))))
+(format t "~%Tiempo ~a: la luz ha cambiado de ~a a ~a" 
+            tiempo 
+            color-actual 
+            (cadr listaColor))))
+
+;; ========================================================
+;; FUNCIÓN: duracion-ciclo
+;; NATURALEZA: Impura
+;; ESTRATEGIA: Secuencial
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun duracion-ciclo (tiempo)
+  (let ((ciclosTotales (floor (/ tiempo 216))))
+    (format t "en ~a segundos se realizan ~a ciclos completos" tiempo ciclosTotales)))
+
+;; ========================================================
+;; FUNCIÓN: recomendacion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Condicional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun recomendacion-ciclo (duracionCiclo)
+  (cond ((< duracionCiclo 35) "El ciclo es muy corto. Aumentar la duracion del ciclo")
+        ((> duracionCiclo 150) "El ciclo es demasiado largo. Disminuir la duracion del ciclo")
+        (t "El ciclo esta en la duracion optima")))
