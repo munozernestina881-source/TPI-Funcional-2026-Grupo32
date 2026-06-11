059fd1bfcaa8472a37f0f05bf01d6d359350de4a
gaston <108883641+qqaston@users.noreply.github.com>
2026-06-11 00:16:37 -0300

    add informe por hora

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -84,3 +84,20 @@
   (let* ((segundos-totales (* minutos 60)) 
          (duracion-ciclo 216)) ; 90s(Rojo) + 6s(Amarillo) + 120s(Verde) 
     (floor (/ segundos-totales duracion-ciclo)))) 
+
+;; =======================================================
+;; FUNCIÓN: informe-por-hora
+;; NATURALEZA: Pura 
+;; ESTRATEGIA: Evaluación simple / secuencial 
+;; IMPACTO: No destructiva
+;; =======================================================
+(defun informe-por-hora ()
+  (let ((total-rojo (* 90 16))
+        (total-amarillo (* 6 16))
+        (total-verde (* 120 16)))
+
+    (let ((tiempo-total (+ total-rojo total-amarillo total-verde)))
+      
+      (list 'rojo '%(* (/ total-rojo tiempo-total) 100.0)
+            'amarillo '%(* (/ total-amarillo tiempo-total) 100.0)
+            'verde '%(* (/ total-verde tiempo-total) 100.0)))))
